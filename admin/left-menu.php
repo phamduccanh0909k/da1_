9cedd68f25e9bdcda04ec60b042f2cec9f60e029
hieuultra <147162531+hieuultra@users.noreply.github.com>
2023-11-05 18:59:28 +0700

    edit anh slide

--- admin/left-menu.php
+++ admin/left-menu.php
@@ -48,6 +48,36 @@
             <a class="nav-link" href="?act=add_pro">Add product</a>
           </nav>
         </div>
+
+        <!-- back to web -->
+        <a class="nav-link collapsed" href="../index.php">
+          <div class="sb-nav-link-icon">
+            <i class="fas fa-columns"></i>
+          </div>
+          Back Website
+          <div class="sb-sidenav-collapse-arrow">
+            <i class="fas fa-angle-down"></i>
+          </div>
+        </a>
+
+        <!-- slider -->
+        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseProduct" aria-expanded="false" aria-controls="collapseLayouts">
+          <div class="sb-nav-link-icon">
+            <i class="fas fa-columns"></i>
+          </div>
+          Slider
+          <div class="sb-sidenav-collapse-arrow">
+            <i class="fas fa-angle-down"></i>
+          </div>
+        </a>
+
+        <div class="collapse" id="collapseProduct" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+          <nav class="sb-sidenav-menu-nested nav">
+            <a class="nav-link" href="?act=list_pro">List Slider</a>
+            <a class="nav-link" href="?act=add_pro">Add Slider</a>
+          </nav>
+        </div>
+
       </div>
 
   </nav>
