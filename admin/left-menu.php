0b0e1d3031c34d68e7c87d9e54b0c0bd52d3c8c2
hieuultra <147162531+hieuultra@users.noreply.github.com>
2023-11-06 22:44:07 +0700

    them ql brand

--- admin/left-menu.php
+++ admin/left-menu.php
@@ -81,6 +81,24 @@ ob_start();
           </nav>
         </div>
 
+        <!-- brand -->
+        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseBrand" aria-expanded="false" aria-controls="collapseLayouts">
+          <div class="sb-nav-link-icon">
+            <i class="fas fa-columns"></i>
+          </div>
+          Brand
+          <div class="sb-sidenav-collapse-arrow">
+            <i class="fas fa-angle-down"></i>
+          </div>
+        </a>
+
+        <div class="collapse" id="collapseBrand" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+          <nav class="sb-sidenav-menu-nested nav">
+            <a class="nav-link" href="?act=list_brand">List Slider</a>
+            <a class="nav-link" href="?act=add_brand">Add Slider</a>
+          </nav>
+        </div>
+
       </div>
 
   </nav>
